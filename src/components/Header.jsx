a199d1c3e9127bbb232c80c9dd6fac02581a3471
Joe McKinney and Robin Sola <joe@>
2018-06-25 10:05:12 -0700

    create first react project

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  let headerStyles = {
+    backgroundColor: 'hotpink',
+    color: 'snow',
+    fontFamily: 'monospace',
+    fontSize: '2em',
+    padding: '30px',
+    textAlign: 'center'
+  }
+  return (
+    <div style={headerStyles}>
+      <h1>Help Queue</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Ticket(props) {
+  let ticketStyles = {
+    fontFamily: 'helvetica',
+    backgroundColor: 'lightyellow',
+    padding: '20px'
+  }
+  return (
+    <div style={ticketStyles}>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
+
+export default Ticket;
